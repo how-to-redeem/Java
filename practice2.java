99678f9ab2c43f992af07422560e22adcd96a1ae
how-to-redeem <wangly0320@163.com>
2019-12-05 22:29:29 +0800

    work

---
+++ practice2.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class practice2 {
+        public static void main(String[] args){
+            int sum = 0;
+            for(int i = 1; i < 101; i++){
+                sum += i;
+            }
+            System.out.println("1到100的和："+sum);
+        }
+    }
+
