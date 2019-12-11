c172272475dd9cbad2cb670ec6f48e0b5fe84085
how-to-redeem <wangly0320@163.com>
2019-12-11 21:00:22 +0800

    work

---
+++ the.java
@@ -0,0 +1,11 @@
+package qqqq;
+
+import java.util.Scanner;
+public class the {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int score = sc.nextInt();
+        }
+    }
+}
